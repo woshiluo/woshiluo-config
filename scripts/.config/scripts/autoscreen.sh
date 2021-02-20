562f1b92e8b335ab11e0f17ffd33dcdfd12deeea
Woshiluo Luo <woshiluo.luo@outlook.com>
2021-02-20 22:05:21 +0800

    fix(autoscreen.sh): Restore from no output

--- scripts/.config/scripts/autoscreen.sh
+++ scripts/.config/scripts/autoscreen.sh
@@ -3,6 +3,8 @@
 while true; do
 	if [ $( swaymsg -r -t get_outputs | jq -r ".[]" --indent 0 | wc -l ) -ge 2 ]; then
 		swaymsg "output eDP-1 disable" >/dev/null 2>&1
+	else 
+		swaymsg "output eDP-1 enable" >/dev/null 2>&1
 	fi
 	sleep 5
 done
